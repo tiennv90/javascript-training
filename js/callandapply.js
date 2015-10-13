3211f582e7317cd6f6617bea683411e55df6464c
tiennv <tiennv90@gmail.com>
2015-10-13 22:08:58 +0700

    finish learning call and apply function

--- js/callandapply.js
+++ js/callandapply.js
@@ -5,3 +5,53 @@ function sqsum(a, b) {
 sqsum(2, 3);
 sqsum.call(null, 2, 3);
 sqsum.apply(null, [2, 3]);
+
+var hello = {
+    name : "Welcomebot",
+    speak : function (to) {
+        return this.name + 'say "welcome, ' + to + '."';
+    }
+}
+
+var goodbye = {
+    name : "Farewellbot",
+    speak : function (to) {
+        return this.name + 'say "Goodbye, ' + to + '."';
+    }
+}
+
+hello.speak('Ethan');
+goodbye.speak('Ethan');
+hello.speak.call(null, 'Ethan');
+hello.speak.call(hello, 'Ethan');
+hello.speak.call(goodbye, 'Ethan');
+hello.speak.apply(goodbye, ['Ethan']);
+goodbye.speak.apply(hello, ['Ethan']);
+
+var speak = hello.speak;
+speak('Ethan');
+speak.call(hello, 'Ethan');
+hello.speak.call({ name : 'AnotherGreetingBot'}, 'Ethan');
+
+function numberRotbot(n) {
+    this.robot = n;
+}
+
+var robots = [hello, goodbye];
+
+for (var i = 0; i < robots.length; i++) {
+    numberRotbot.call(robots[i], i);
+}
+
+hello;
+goodbye;
+
+var robotRegistry = {
+    robots : [],
+    addRobot : function (r) {
+        this.robots.push(r);
+    }
+}
+
+robots.forEach(robotRegistry.addRobot, robotRegistry);
+robotRegistry.robots;
